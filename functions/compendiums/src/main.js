5f6a2f400034f9483e592259853ca9c79929a497
mlstanley <mlstanley@gmail.com>
2024-02-13 20:23:16 -0800

    init

--- functions/compendiums/src/main.js
+++ functions/compendiums/src/main.js
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import { Client } from 'node-appwrite';
 
 // This is your Appwrite function
@@ -5,10 +6,10 @@ import { Client } from 'node-appwrite';
 export default async ({ req, res, log, error }) => {
   // Why not try the Appwrite SDK?
   //
-  // const client = new Client()
-  //   .setEndpoint('https://cloud.appwrite.io/v1')
-  //   .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //   .setKey(process.env.APPWRITE_API_KEY);
+  const client = new Client()
+    .setEndpoint(process.env.APPWRITE_ENDPOINT)
+    .setProject(process.env.APPWRITE_PROJECT_ID)
+    .setKey(process.env.APPWRITE_KEY);
 
   // You can log messages to the console
   log('Hello, Logs!');
