a1e4792d8fde3a1ea13bb189a0e193264aab9839
mlstanley <mlstanley@gmail.com>
2024-02-14 10:53:47 -0800

    init

---
+++ functions/compendiums/src/utils/awClient.js
@@ -0,0 +1,9 @@
+import { Client, Databases } from 'node-appwrite';
+
+const client = new Client()
+    .setEndpoint(process.env.APPWRITE_ENDPOINT)
+    .setProject(process.env.APPWRITE_PROJECT_ID)
+    .setKey(process.env.APPWRITE_KEY);
+export function databases() {
+    return new Databases(client)
+}
