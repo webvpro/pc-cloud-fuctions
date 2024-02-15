14b07b3cac53c12c600ca5ff58f278eb847b4b25
mlstanley <mlstanley@gmail.com>
2024-02-14 21:34:58 -0800

    init

--- functions/compendiums/src/main.js
+++ functions/compendiums/src/main.js
@@ -13,6 +13,7 @@ export default async ({ req, res, log, error, context }) => {
   const client = new Client()
     .setEndpoint(process.env.APPWRITE_ENDPOINT)
     .setProject(process.env.APPWRITE_PROJECT_ID)
+    .setKey(process.env.APPWRITE_KEY) 
     const databases = new Databases(client);
     const storage = new Storage(client);
     const account = new Account(client);
